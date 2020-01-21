e597c87f430d36bef213a6cdb59bddfd05c17a65
Romanov-aa <Lexaakm@mail.ru>
2020-01-21 12:21:34 +0300

    sdeal s 7 po 10

--- task_10.py
+++ task_10.py
@@ -5,6 +5,17 @@ from pyrob.api import *
 
 @task
 def task_8_3():
+    while not wall_is_on_the_right():
+        if not wall_is_above() and not wall_is_beneath():
+            
+            move_right()
+        else:
+            fill_cell()
+            move_right()
+            
+    if wall_is_above() or wall_is_beneath():
+        fill_cell()
+        
     pass
 
 
